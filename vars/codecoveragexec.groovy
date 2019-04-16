c4d5eb9a9efb436a41ada80b08ca9afedfd17389
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-04-17 04:19:05 +0530

    Create codecoveragexec.groovy

---
+++ vars/codecoveragexec.groovy
@@ -0,0 +1,7 @@
+def call()
+{
+def Sonarscanner = tool 'Sonarscanner';
+     withSonarQubeEnv('Sonarqube') {
+    sh  """${Sonarscanner}/sonar-scanner -Dsonar.host.url=http://ec2-34-244-155-32.eu-west-1.compute.amazonaws.com -Dsonar.login=admin -Dsonar.password=soumianisoumya@123 -Dsonar.jacoco.reportPaths=target/jacoco.exec"""
+     }
+}
