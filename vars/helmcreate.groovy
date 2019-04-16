7776b063f68d0af386bff1c3a15fe0015395020c
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-04-16 12:32:58 +0530

    Update helmcreate.groovy

--- vars/helmcreate.groovy
+++ vars/helmcreate.groovy
@@ -1,10 +1,10 @@
-def call (String ${microserviceName}, String ${container_port}){
+def call (String ${microserviceName}, String ${container_port}, String ${dockerImage}){
 
 #mod helm chart
 sh """
 cat <<EOF > helmcreate.sh
 #!/bin/bash
-sed -i "s/nginx/${image}/g" ${microserviceName}/values.yaml
+sed -i "s/nginx/${dockerImage}/g" ${microserviceName}/values.yaml
 sed -i "s/stable/latest/g" ${microserviceName}/values.yaml
 sed -i "s/80/${container_port}/g" ${microserviceName}/templates/deployment.yaml
 EOF"""
