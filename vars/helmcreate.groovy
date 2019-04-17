c9e33548023c823735da90095201acc5e55ad949
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-04-17 09:55:57 +0530

    Update helmcreate.groovy

--- vars/helmcreate.groovy
+++ vars/helmcreate.groovy
@@ -1,7 +1,7 @@
 def call (String microserviceName){
 
 
-	sh "echo ${microserviceName} ${prop['deploy.container_port']} ${prop['deploy.dockerImage']}"
+	sh "echo ${microserviceName} ${props['deploy.container_port']} ${prop['deploy.dockerImage']}"
 sh """
 sed -i "s/nginx/${props['deploy.dockerImage']} /g" ${microserviceName}/values.yaml
 sed -i "s/stable/latest/g" ${microserviceName}/values.yaml
