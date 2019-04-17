599842017ff8c377f2d5388004d1765f694e185b
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-04-17 11:10:22 +0530

    Update sonarexec.groovy

--- vars/sonarexec.groovy
+++ vars/sonarexec.groovy
@@ -1,7 +1,8 @@
-def call() {
+def call(String s) {
        def Sonarscanner = tool 'Sonarscanner';
     withSonarQubeEnv('Sonarqube') 
     {
+           sh "echo ${s}" 
         sh """echo ${Sonarscanner}"""
      sh  """${Sonarscanner}/sonar-scanner -Dsonar.host.url=http://ec2-34-244-155-32.eu-west-1.compute.amazonaws.com -Dsonar.login=admin -Dsonar.password=soumianisoumya@123"""
         
