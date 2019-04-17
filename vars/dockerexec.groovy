49cf10cbd692b88cc4c3cc563a967235855b3667
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-04-17 12:23:21 +0530

    Update dockerexec.groovy

--- vars/dockerexec.groovy
+++ vars/dockerexec.groovy
@@ -2,6 +2,5 @@ def call(String microserviceName){
   dockerImage = docker.build("saumyaprashar/${microserviceName}:${BUILD_NUMBER}") 
   dockerImage1 = "saumyaprashar/${microserviceName}"
   sh "docker images"
-  sh "echo ${dockerImage1}"
-  return dockerImage1
+  return dockerImage
   }
