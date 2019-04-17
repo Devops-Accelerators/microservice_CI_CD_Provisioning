263452d2d63cb75dd3381464c28f5e06d0226af1
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-04-17 11:39:24 +0530

    Update helmcreate.groovy

--- vars/helmcreate.groovy
+++ vars/helmcreate.groovy
@@ -3,9 +3,9 @@ def call (String dockerImage){
 
 	
 	prop = readProperties  file: """deploy.properties"""
-	sh" echo ${dockerImage}
-	 echo prop['deploy.microservice']
-	"
+	sh" echo ${dockerImage}"
+	sh" echo prop['deploy.microservice']"
+	
 /*sh """
 sed -i "s/nginx/${dockerImage} /g" prop['deploy.microservice']/values.yaml
 sed -i "s/stable/latest/g" prop['deploy.microservice']/values.yaml
