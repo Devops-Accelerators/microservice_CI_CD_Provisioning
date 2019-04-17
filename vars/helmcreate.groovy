cc710c85819b50990ac3f5ac6c5ae23a8f40bafa
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-04-17 12:22:39 +0530

    Update helmcreate.groovy

--- vars/helmcreate.groovy
+++ vars/helmcreate.groovy
@@ -5,7 +5,7 @@ def call (String dockerImage){
 	sh" echo ${dockerImage}"
 	
 sh """
-sed -i "s/nginx/${dockerImage} /g" helmchart/values.yaml
+sed -i "s/nginx/saumyaprashar/${dockerImage} /g" helmchart/values.yaml
 """
 	 
   }
