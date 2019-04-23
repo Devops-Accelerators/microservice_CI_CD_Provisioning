8f320c20cf90e346af2fa33d1461dc4ef7f9e0ba
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-04-23 17:12:08 +0530

    Update mavenbuildexec.groovy

--- vars/mavenbuildexec.groovy
+++ vars/mavenbuildexec.groovy
@@ -1,6 +1,6 @@
 def call(String arg){
         echo "${arg}"
-withEnv( ["PATH+MAVEN=${tool mvn_version}/bin"] ) { 
+withEnv( ["PATH+MAVEN=${tool mvn1}/bin"] ) { 
    
         sh "mvn clean install" 
     } 
