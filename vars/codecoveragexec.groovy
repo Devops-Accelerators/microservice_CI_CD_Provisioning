db7f60bddd3cd7f668f2025095378fd4fca93c9b
DevOps-Accelerators <48236651+Devops-Accelerators@users.noreply.github.com>
2019-04-23 17:08:16 +0530

    Update codecoveragexec.groovy

--- vars/codecoveragexec.groovy
+++ vars/codecoveragexec.groovy
@@ -3,6 +3,6 @@ def call(String str)
      sh "echo ${str}"
 def Sonarscanner = tool 'Sonarscanner';
      withSonarQubeEnv('Sonarqube') {
-    sh  """${Sonarscanner}/sonar-scanner -Dsonar.host.url=http://ec2-34-244-155-32.eu-west-1.compute.amazonaws.com -Dsonar.login=admin -Dsonar.password=soumianisoumya@123 -Dsonar.jacoco.reportPaths=target/jacoco.exec"""
+    sh  """${Sonarscanner}/sonar-scanner -Dsonar.host.url=http://ec2-54-229-131-76.eu-west-1.compute.amazonaws.com -Dsonar.login=admin -Dsonar.password=soumianisoumya@123 -Dsonar.jacoco.reportPaths=target/jacoco.exec"""
      }
 }
