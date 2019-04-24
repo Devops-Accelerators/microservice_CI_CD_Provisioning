512f782b8bc0e9b6b658805d512151284c15886e
DevOps-Accelerators <sasdevops@gmail.com>
2019-04-24 12:12:18 +0530

    Create helmdeploy.groovy

---
+++ vars/helmdeploy.groovy
@@ -0,0 +1,35 @@
+def call (String microserviceName){
+
+    sh """
+cat <<EOF > rbac-config.yaml
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: tiller
+  namespace: kube-system
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: tiller
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+  - kind: ServiceAccount
+    name: tiller
+    namespace: kube-system
+EOF
+
+kubectl apply -f rbac-config.yaml
+
+helm init --service-account tiller --upgrade
+
+rm rbac-config.yaml
+
+helm install -n ${miroserviceName} helmchart --dry-run --debug
+"""
+    
+
+}
