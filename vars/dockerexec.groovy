8c9bb605d817de761ce6924459ba508731fbd04d
DevOps-Accelerators <sasdevops@gmail.com>
2019-04-25 10:49:01 +0530

    Update dockerexec.groovy

--- vars/dockerexec.groovy
+++ vars/dockerexec.groovy
@@ -1,5 +1,5 @@
-def call(String microserviceName){
-  dockerImage = docker.build("devopsaccelerator/${microserviceName}:${BUILD_NUMBER}")
+def call(String imageName){
+  dockerImage = docker.build("${imageName}:${BUILD_NUMBER}")
   sh "docker images"
   return dockerImage
   }
