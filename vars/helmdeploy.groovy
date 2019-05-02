24cf404251a96a9ca152ba29455e88a409c0c8a8
DevOps-Accelerators <sasdevops@gmail.com>
2019-05-02 16:39:52 +0530

    Update helmdeploy.groovy

--- vars/helmdeploy.groovy
+++ vars/helmdeploy.groovy
@@ -1,6 +1,36 @@
 def call(String s) {
 
-    sh "echo $s"
-    sh "ls -al"
-    sh "helm install helmchart"
+    sh """
+cat <<EOF > rbac-config.yaml
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: tiller
+  namespace: kube-system
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: tiller
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+  - kind: ServiceAccount
+    name: tiller
+    namespace: kube-system
+EOF
+
+kubectl apply -f rbac-config.yaml
+
+helm init --service-account tiller --upgrade
+
+rm rbac-config.yaml
+
+sleep 10
+
+helm install helmchart --dry-run
+    
+    """
 }
